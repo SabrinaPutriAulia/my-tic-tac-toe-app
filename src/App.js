6abd926450d00e9995379355740600eb550e0033
SabrinaPutriAulia <21081010048@student.upnjatim.ac.id>
2023-11-04 15:29:48 +0700

    React JS

--- src/App.js
+++ src/App.js
@@ -1,43 +1,58 @@
-
-import * as React from 'react';
+import * as React from "react";
 
 function Board() {
-  const squares = Array(9).fill(null);
-  function selectSquare(square) {
+  const [squares, setSquares] = React.useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = React.useState(true);
 
+  function selectSquare(square) {
+    if (calculateWinner(squares) || squares[square]) {
+      return;
+    }
+    const squaresCopy = squares.slice();
+    squaresCopy[square] = xIsNext ? "X" : "O";
+    setSquares(squaresCopy);
+    setXIsNext(!xIsNext);
   }
 
   function restart() {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
   }
 
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <button
+        className="bg-white border border-gray-300 w-20 h-20 text-4xl font-semibold"
+        onClick={() => selectSquare(i)}
+      >
         {squares[i]}
       </button>
     );
   }
 
+  const winner = calculateWinner(squares);
+  const nextValue = calculateNextValue(squares);
+  const status = calculateStatus(winner, squares, nextValue);
+
   return (
-    <div>
-      <div >STATUS</div>
-      <div >
+    <div className="flex flex-col items-center">
+      <div className="text-2xl font-semibold mb-10 text-white">{status}</div>
+      <div className="grid grid-cols-3 gap-4">
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
-      </div>
-      <div >
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
-      </div>
-      <div >
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button onClick={restart}>
-        restart
+      <button
+        className="bg-blue-500 text-white px-12 py-3 mt-10 rounded-full"
+        onClick={restart}
+      >
+        Restart
       </button>
     </div>
   );
@@ -45,8 +60,8 @@ function Board() {
 
 function Game() {
   return (
-    <div >
-      <div >
+    <div className="flex justify-center items-center h-screen">
+      <div>
         <Board />
       </div>
     </div>
@@ -58,13 +73,13 @@ function calculateStatus(winner, squares, nextValue) {
   return winner
     ? `Winner: ${winner}`
     : squares.every(Boolean)
-      ? `Scratch: Cat's game`
-      : `Next player: ${nextValue}`;
+    ? `Scratch: Cat's game`
+    : `Next player: ${nextValue}`;
 }
 
 // eslint-disable-next-line no-unused-vars
 function calculateNextValue(squares) {
-  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
+  return squares.filter(Boolean).length % 2 === 0 ? "X" : "O";
 }
 
 // eslint-disable-next-line no-unused-vars
